540e2e956e9a0f4dbf35dd4ebc86f9765fa21284
Alang Kim <rlaangh77@naver.com>
2023-11-09 02:03:25 +0900

    fix: BuildShipView story 수정

--- modules/battle-ship/view/BattleShipView.stories.tsx
+++ modules/battle-ship/view/BattleShipView.stories.tsx
@@ -1,6 +1,8 @@
 import { type } from "os";
 import { BattleShipView } from "./BattleShipView";
 import { StoryObj } from "@storybook/react";
+import { BattleShip } from "../model/BattleShip";
+import { Coordinate } from "@/modules/engine/model/Coordinate";
 
 export default {
   title: "BattleShip",
@@ -11,6 +13,15 @@ type Story = StoryObj<typeof BattleShipView>;
 
 export const Default: Story = {
   args: {
-    status: "alive",
+    model: new BattleShip({
+      color: "red",
+    }),
   },
+  decorators: [
+    (Story) => (
+      <div style={{ width: "100px", height: "100px" }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
