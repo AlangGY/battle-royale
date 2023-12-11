9c32a90ca903d46722e38c6c5067d59781e88d78
Alang Kim <rlaangh77@naver.com>
2023-12-12 01:24:29 +0900

    implement: RoomsFetcher 작성

---
+++ modules/api/fetcher/RoomsFetcher.ts
@@ -0,0 +1,26 @@
+import { RealtimePostgresChangesPayload } from "@supabase/supabase-js";
+import supabaseClient from "../SupabaseClient";
+import { RoomResponse } from "../schemas";
+
+export class RoomsFetcher {
+  static async get() {
+    const { data: rooms, error } = await supabaseClient
+      .from("rooms")
+      .select<"*", RoomResponse>("*");
+
+    return { rooms, error };
+  }
+
+  static subscribe(
+    callback: (e: RealtimePostgresChangesPayload<RoomResponse>) => void
+  ) {
+    return supabaseClient
+      .channel("rooms")
+      .on<RoomResponse>(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "rooms" },
+        callback
+      )
+      .subscribe();
+  }
+}
---
+++ modules/api/schemas.ts
@@ -0,0 +1,6 @@
+export interface RoomResponse {
+  id: number;
+  name: string;
+  created_at: string;
+  players: any[];
+}
